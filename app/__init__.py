b4e7a1c4dee29dee0482ceaac82b02297be9d287
YoustinaZak <tina.magdi.tm@gmail.com>
2026-08-09 03:59:47 +0300

    migrating db

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from app.config import Config
 
 db = SQLAlchemy()
-
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    migrate.init_app(app, db)
 
     db.init_app(app)
 
