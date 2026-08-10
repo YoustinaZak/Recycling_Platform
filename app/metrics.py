8b4000afc3aef9432a5248533db40f4dc6f59c2e
YoustinaZak <tina.magdi.tm@gmail.com>
2026-08-10 09:41:30 +0300

    metrics code rewritten

--- app/metrics.py
+++
@@ -1,6 +0,0 @@
-from prometheus_client import Gauge
-
-queue_length = Gauge(
-    "recycling_queue_length",
-    "Current number of events waiting in the Redis queue",
-)
--- worker/metrics.py
+++ worker/metrics.py
@@ -1,4 +1,4 @@
-from prometheus_client import Counter
+from prometheus_client import Counter, Gauge
 
 events_processed = Counter(
     "recycling_events_processed_total",
@@ -8,4 +8,9 @@ events_processed = Counter(
 events_failed = Counter(
     "recycling_events_failed_total",
     "Number of recycling events that failed",
+)
+
+queue_length = Gauge(
+    "recycling_queue_length",
+    "Current number of events waiting in the Redis queue",
 )
